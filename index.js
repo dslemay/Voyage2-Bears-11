5e8087d3fddaba49b9098d432b901a7045a86243
Daniel Lemay <d.s.lemay@icloud.com>
2017-09-30 09:30:43 -0700

    Add environment variable import for Node Server
    The Node application now imports environment variables from
    variables.env on the root level of the project.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// Import Environment variables to store information not in the repo.
+require("dotenv").config({ path: "variables.env" });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
