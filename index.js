027f951073e5158485360108fcc960b29a980599
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-01 10:38:42 -0700

    Add body-parser and express-validator packages
    These packages were added to the package.json file as dependencies amd
    initialized in index.js. These dependencies will be needed for parsing
    the form submissions for user registration and login.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const routes = require('./routes/authRoutes');
 
 const app = express();
 
 // Import Environment variables to store information not in the repo.
-require("dotenv").config({ path: "variables.env" });
+require('dotenv').config({ path: 'variables.env' });
 
 // Connect to Database and use native ES6 promises
 mongoose.Promise = global.Promise;
@@ -14,8 +17,19 @@ mongoose
   })
   .catch(err => console.error(`Unable to connect to MongoDB: ${err.message}`));
 
+// Set up middlewares
+
+//Body Parser to be used for passing the fields for login and registration form
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Validator will be used for registration form to validate and sanitize data
+app.use(expressValidator());
+
+app.use('/', routes);
+
 // Determine port and start server.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is running...");
+  console.log('Server is running...');
 });
