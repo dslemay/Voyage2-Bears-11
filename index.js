6b61dcc99f3a2444669cc42b08d050dfcb7f5ca2
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-01 08:14:05 -0700

    Update env filename to remove variables from it
    The server filename was updated to .env. The configuration to load the
    file in index.js was updated to reflect this change, and the README
    now provides updated instructions about the file.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // Import Environment variables to store information not in the repo.
-require("dotenv").config({ path: "variables.env" });
+require('dotenv').config();
 
 // Connect to Database and use native ES6 promises
 mongoose.Promise = global.Promise;
@@ -17,5 +17,5 @@ mongoose
 // Determine port and start server.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is running...");
+  console.log('Server is running...');
 });
