4f24bc347578263b1910775f3e7003edf141ecc2
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-01 08:48:06 -0700

    Add initial User schema for Mongoose
    The schema currently holds the email and name fields for the database.
    Future updates will add storing of favorite places and POIs.

--- models/User.js
+++ models/User.js
@@ -1,1 +1,25 @@
 // Add user schema with mongoose here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+import isEmail from 'validator/lib/isEmail';
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+
+const userScema = new Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: [isEmail, 'Please supply a valid email'],
+    required: 'Please supply an email'
+  },
+  name: {
+    type: String,
+    trime: true,
+    required: 'Please supply a name'
+  }
+});
+
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
