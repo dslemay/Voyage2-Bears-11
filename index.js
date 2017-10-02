65a79a77a7b15036d2cd22ddca90ed923024f837
Jarold Wong <jaroldw@gmail.com>
2017-10-02 11:50:01 -0700

    POST login form data

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Express server running');
+});
+
+app.post('/login', function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  res.send('Success');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
