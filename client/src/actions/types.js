6fae0ca7f3d6180db33163f650eb144fa0e82cda
Alexander Steele <alexsteele110@gmail.com>
2017-10-03 15:12:36 -0400

    added redux and redux-thunk

---
+++ client/src/actions/types.js
@@ -0,0 +1,3 @@
+// Where we define action types
+// For example
+// export const FETCH_USER = 'fetch-user';
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,18 @@ import 'materialize-css/dist/js/materialize.min.js';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Once we add reducers, replace first argument of createStore with 'reducers'
+// and import reducers folder here
+const store = createStore(() => [], {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
