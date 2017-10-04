3428518e8f136e4adbcadb10a28c40d96eb2ef3d
Alexander Steele <alexsteele110@gmail.com>
2017-10-04 15:30:01 -0400

    successful yelp api request with node helper library

--- routes/detailRoutes.js
+++ routes/detailRoutes.js
@@ -1,52 +1,26 @@
 const axios = require('axios');
+const yelp = require('yelp-fusion');
 const keys = require('../config/keys');
 
-module.exports = app => {
-  app.get('api/yelp', (req, res) => {
-    const lat = 37.8136;
-    const lng = 144.9631;
+const clientId = keys.yelpClientID;
+const clientSecret = keys.yelpClientSecret;
 
-    axios({
-      method: 'post',
-      url: 'https://api.yelp.com/oauth2/token',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      params: {
-        grant_type: 'client_credentials',
-        client_id: keys.yelpClientID,
-        client_secret: keys.yelpClientSecret
-      }
-    })
-      .then(token => {
-        const yelpToken = token.data.access_token;
-        return getHotels(null, lat, lng);
-      })
-      .then(results => {
-        res.status(201).send(results);
-      })
-      .catch(err => {
-        res.status(err.status).send(err);
-      });
-  });
-  function getHotels(token, lat, lng) {
-    if (token) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    } else {
-      console.log('Problem retrieving hotels...');
-    }
-    return axios({
-      method: 'get',
-      url: 'https://api.yelp.com/v3/businesses/search',
-      params: {
-        term: 'hotels',
-        latitude: lat,
-        longitude: lng
-      }
-    })
-      .then(results => {
-        return results.data;
-      })
-      .catch(err => {
-        console.log('Error');
-      });
-  }
+const searchRequest = {
+  term: 'Hotels',
+  location: 'san francisco, ca'
 };
+
+yelp
+  .accessToken(clientId, clientSecret)
+  .then(response => {
+    const client = yelp.client(response.jsonBody.access_token);
+
+    client.search(searchRequest).then(response => {
+      const firstResult = response.jsonBody.businesses[0];
+      const prettyJson = JSON.stringify(firstResult, null, 4);
+      console.log(prettyJson);
+    });
+  })
+  .catch(e => {
+    console.log(e);
+  });
