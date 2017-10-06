9c343ed28d1d38d8d2ed3fae8d88d0b4ba87663f
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-05 17:40:39 -0700

    Add username field config to Passport Strategy
    The usernameField option was passed to the Passport Strategy
    configuration and set to 'email'. This is necessary due to the form
    field being passed to the authentication is named 'email'.

--- services/passport.js
+++ services/passport.js
@@ -6,18 +6,23 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 
 passport.use(
-  new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      return done(null, user);
-    });
-  })
+  new LocalStrategy(
+    {
+      usernameField: 'email'
+    },
+    function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+        return done(null, user);
+      });
+    }
+  )
 );
