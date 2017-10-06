5af5e2451c004ed9f06eeb6cba8d91f9dbdc1959
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-06 13:21:35 -0700

    Add initial structure to post registration form
    The intial route for posting the registration form has been added and
    the authController imported. Currently, the validate register is the
    only method which has been created in the registration flow.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,13 @@
 // Add logic for authorization routes here
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.post(
+  '/register',
+  authController.validateRegister
+  // authController.register,
+  // authController.login
+);
+
+module.exports = router;
