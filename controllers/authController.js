cd0fb881a94ecceae3e4351ce26711312e827c34
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-07 14:22:52 -0700

    Update the validateRegister function
    The validate register function was not formatted properly. In order to
    make it work properly, it was broken out into two separate exports:
    'validateRegister' and 'checkValidations.' The validateRegister runs
    the validation tests against the form registration. After this is
    completed the checkValidations function will be called which will see
    if there are any errors. If there are errors, they will be sent to the
    user. If there are not, the chain will continue.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,38 @@
+const { check, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const passport = require('passport');
 const mongoose = require('mongoose');
-const User = mongoose.Model('User');
+const User = mongoose.model('User');
 
-exports.validateRegister = (req, res, next) => {
-  // Sanitize name and check that it is not empty
+exports.validateRegister = [
+  // Check that the name is not empty and sanitize the data
   check('name', 'You must supply a name.')
-    .exists()
-    .trim();
-  sanitizeBody('name');
-  // Sanitize email, make sure it is valid, and normalize?
+    .not()
+    .isEmpty()
+    .trim(),
+  sanitizeBody('name').escape(),
+  // Check that the email is valid and normalize
   check('email', 'You must supply an email')
     .isEmail()
-    .trim();
-  sanitizeBody('email').normalizeEmail();
-  // Check that password is not empty.
-  check('password', 'You must supply a password').exists();
-  // Check that password confirm is not empty and is the same as password
-  // check('password-confirm', 'You must confirm your password')
-  //   .exists()
-  //   .custom((value, { req }) => value === req.body.password);
-  // Check for errors. Call next if no errors. If errors return and display errors.
+    .trim(),
+  sanitizeBody('email').normalizeEmail(),
+  // Check that the password is not empty
+  check('password', 'You must supply a password')
+    .not()
+    .isEmpty()
+  // Check that the password confirm field is not empty and the passwords match
+  // TODO: Uncomment this logic when the password-confirm is added to the front-end
+  // check('password-confirm')
+  //   .not()
+  //   .isEmpty()
+  //   .withMessage('You must confirm your password')
+  //   .custom((value, { req }) => value === req.body.password)
+  //   .withMessage('Your passwords must match')
+];
+
+exports.checkValidations = (req, res, next) => {
   const errors = validationResult(req);
-  if (errors) {
+  if (!errors.isEmpty()) {
     return res.json({ errors: errors.mapped() });
   }
   next();
