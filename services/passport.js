12528fe70c90fac8e996e3c7fae78667dbe23eba
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-09 20:17:58 -0700

    Implement Passport-local-mongoose package
    This package was implemented to streamline the passport local
    functionality and allow more streamlined registration workflow including
    the password hashing.

--- services/passport.js
+++ services/passport.js
@@ -1,28 +1,10 @@
 // Create the strategy for passport authentication
 
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-const User = require('../models/User');
+const User = mongoose.model('User');
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'email'
-    },
-    function(username, password, done) {
-      User.findOne({ username: username }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password' });
-        }
-        return done(null, user);
-      });
-    }
-  )
-);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
