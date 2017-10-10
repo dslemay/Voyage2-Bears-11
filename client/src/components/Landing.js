eac4ecda6723991689ac0a1fcaee56777c940031
Alexander Steele <alexsteele110@gmail.com>
2017-10-10 16:40:06 -0400

    added grid system and main card

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,83 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 
-const Landing = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+    marginRight: 100,
+    marginLeft: 100
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  media: {
+    height: 400
+  }
+});
+
+function Landing(props) {
+  const { classes } = props;
+
   return (
-    <div>
-      <h1>Landing</h1>
-      <h2>Welcome to our travel site!</h2>
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image="https://source.unsplash.com/IE_sifhay7o"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography type="display2" component="h2">
+                Welcome
+              </Typography>
+              <Typography component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button raised color="primary">
+                Generate Destinations
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
+}
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default Landing;
+export default withStyles(styles)(Landing);
