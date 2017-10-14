32bae19c555393832185d5e079daa39dd6a3aec3
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-14 12:26:25 -0700

    Add clarifying comment about passport config

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
+// Method exposed under Passport-local-mongoose. Runs default Passport config
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
