6b0bca32fdf8892ef9c1e31fae5233273b110b66
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-22 08:39:08 -0700

    Add route for fetching current user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,8 +11,13 @@ module.exports = app => {
   );
 
   app.post('/auth/login', authController.login);
-  app.get('/auth/logout', (req, res) => {
+
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.redirect('/');
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
   });
 };
