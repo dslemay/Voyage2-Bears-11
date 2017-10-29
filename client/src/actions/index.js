def168b9fef477ab7a824a77048a9fead035f921
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-28 19:17:21 -0700

    Add actions and types for adding a location
    The logic in the action creator will add a favorite if there was no
    index location in the user array.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 // Where we put our action creators
 import axios from 'axios';
-import { FETCH_HOTELS, FETCH_USER } from './types';
+import { FETCH_HOTELS, FETCH_USER, ADD_FAVORITE } from './types';
 
 export const fetchHotels = () => async dispatch => {
   const res = await axios.get('/api/yelp');
@@ -12,4 +12,19 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const updateFavorites = (favArrName, locationId) => async dispatch => {
+  const res = await axios.post(`/api/favorites/${favArrName}`, { locationId });
+  const index = res.data.index;
+
+  if (!index) {
+    return dispatch({
+      type: ADD_FAVORITE,
+      favArrName,
+      locationId,
+      index: res.data.index
+    });
+  }
+  // ADD LOGIC FOR DELETING ITEMS
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,3 @@
 export const FETCH_HOTELS = 'fetch_hotels';
 export const FETCH_USER = 'fetch-user';
+export const ADD_FAVORITE = 'add-favorite';
