c348172e5d0db8eddb1ea7a21defb82da7fe012b
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-28 19:22:16 -0700

    Add storing index and return value
    The updateFavorites method now finds the index of the location passed
    to it and sets it to undefined if the location doesn't exist in the
    array. Once the User model has been completed it now sends the index
    value back to the Redux action.

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -9,9 +9,11 @@ const clientSecret = keys.yelpClientSecret;
 exports.updateFavorites = async (req, res, next) => {
   // Check if place ID exists in current user array. If it doesn't $addToSet. If it does, remove from array
   const userLocationArr = req.params.type; // Which array we are querying
-  const locationQuery = req.body.location; // Location to be adding or removing
+  const locationQuery = req.body.locationId; // Location to be adding or removing
   const favorites = req.user[userLocationArr];
   const operator = favorites.includes(locationQuery) ? '$pull' : '$addToSet';
+  const favIndex = favorites.indexOf(locationQuery);
+  const index = favIndex > -1 ? favIndex : undefined;
 
   await User.findByIdAndUpdate(
     req.user._id,
@@ -21,5 +23,5 @@ exports.updateFavorites = async (req, res, next) => {
     { new: true }
   );
 
-  next();
+  res.send({ index });
 };
