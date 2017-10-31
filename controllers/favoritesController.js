cda83a774f6785622d5a33441f4dd9a8a47d3d5c
Daniel Lemay <d.s.lemay@icloud.com>
2017-10-30 19:21:28 -0700

    Update structure of updateFavorites method
    The structure was updated to reference the nested favorites object
    contained in the User model.

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -8,9 +8,10 @@ const clientSecret = keys.yelpClientSecret;
 
 exports.updateFavorites = async (req, res) => {
   // Check if place ID exists in current user array. If it doesn't $addToSet. If it does, remove from array
-  const userLocationArr = req.params.type; // Which array we are querying
+  const type = req.params.type;
+  const userLocationArr = 'favorites.' + req.params.type; // Which array we are querying
   const locationQuery = req.body.locationId; // Location to be adding or removing
-  const favorites = req.user[userLocationArr];
+  const favorites = req.user.favorites[type];
   const operator = favorites.includes(locationQuery) ? '$pull' : '$addToSet';
   const favIndex = favorites.indexOf(locationQuery);
   const index = favIndex > -1 ? favIndex : undefined;
