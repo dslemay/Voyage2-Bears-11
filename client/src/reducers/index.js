679f2da0730178ee1c3c911e1ec5d3671eaf1411
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-04 08:22:13 -0700

    Implement messages state in Redux
    The actions and reducer for storing error/success messages to the user
    were created and implemented in the project.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import hotelsReducer from './hotelsReducer';
 import authReducer from './authReducer';
 import favoritesReducer from './favoritesReducer';
+import messageReducer from './messageReducer';
 
 export default combineReducers({
   hotels: hotelsReducer,
   auth: authReducer,
-  favorites: favoritesReducer
+  favorites: favoritesReducer,
+  messages: messageReducer
 });
