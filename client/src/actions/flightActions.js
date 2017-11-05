211a0b0bed43d168387c74e31c5dc20974ac867b
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-04 22:19:14 -0700

    Update flight actions to use back end API
    The fetch flights actions for Google flights now pings a route on the
    back end to handle making the request to Google. It then returns the
    response object to the action to dispatch to the Redux store.

--- client/src/actions/flightActions.js
+++ client/src/actions/flightActions.js
@@ -1,39 +1,12 @@
 import { FETCH_FLIGHTS } from './types';
 import axios from 'axios';
 
-// Action Creator
-export function fetchFlightsSuccess(flights) {
-  return { type: FETCH_FLIGHTS, flights }; // or flights: flights
-}
+export const fetchFlights = (origin, destination, date) => async dispatch => {
+  const flights = await axios.post('/api/flights', {
+    origin,
+    destination,
+    date
+  });
 
-// Thunk
-// post flight info to google api
-export function fetchFlights() {
-  const url =
-    'https://www.googleapis.com/qpxExpress/v1/trips/search?key=';
-  const data = {
-    request: {
-      slice: [
-        {
-          origin: 'LAS',
-          destination: 'SFO',
-          date: '2017-11-30'
-        }
-      ],
-      passengers: {
-        adultCount: 1
-      },
-      solutions: 1
-    }
-  };
-  return function(dispatch) {
-    return axios
-      .post(url, data)
-      .then(flights => {
-        dispatch(fetchFlightsSuccess(flights));
-      })
-      .catch(error => {
-        throw error;
-      });
-  };
-}
+  dispatch({ type: FETCH_FLIGHTS, flights });
+};
--- routes/detailRoutes.js
+++ routes/detailRoutes.js
@@ -1,8 +1,10 @@
+const axios = require('axios');
 const yelp = require('yelp-fusion');
 const keys = require('../config/keys');
 
 const clientId = keys.yelpClientID;
 const clientSecret = keys.yelpClientSecret;
+const googleFlights = keys.googleFlights;
 
 const searchRequest = {
   term: 'Hotels',
@@ -27,5 +29,33 @@ module.exports = app => {
       .catch(e => {
         console.log(e);
       });
+  });
+
+  app.post('/api/flights', async (req, res) => {
+    const url = `https://www.googleapis.com/qpxExpress/v1/trips/search?key=${googleFlights}`;
+    const origin = req.body.origin;
+    const destination = req.body.destination;
+    const date = req.body.date;
+    const data = {
+      request: {
+        slice: [
+          {
+            origin,
+            destination,
+            date
+          }
+        ],
+        passengers: {
+          adultCount: 1
+        },
+        solutions: 1
+      }
+    };
+    try {
+      const flights = await axios.post(url, data);
+      res.send({ flights: flights.data });
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
