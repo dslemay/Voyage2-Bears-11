1ddd7f6e8cdc252738487fb7205dcdb6f43a6f55
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-08 18:54:07 -0800

    Connect DestinationDetails Component to Redux store
    The component now receives its data from the destination state stored
    in Redux. Upon the component mounting it calls the fetchDestination
    action with the slug passed in as a route param. Due to a slight delay
    in this data being populated, a renderContent function was created
    which inserts in all of the data in the component. When the destination
    props is null the page will render blank. As soon as the data is
    populated, the entire page is rendered out.

--- client/src/components/details/DestinationDetails.js
+++ client/src/components/details/DestinationDetails.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import DetailsTab from './DetailsTab';
-import destinationInformation from '../destinationInformation';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
@@ -8,6 +8,7 @@ import Grid from 'material-ui/Grid';
 import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
+import * as actions from '../../actions';
 
 const styles = theme => ({
   root: {
@@ -31,49 +32,66 @@ const styles = theme => ({
   }
 });
 
-function DestinationDetails(props) {
-  const { classes } = props;
-  const destination = destinationInformation[props.match.params.number];
+class DestinationDetails extends Component {
+  componentDidMount() {
+    const destination = this.props.match.params.location;
+    this.props.fetchDestination(destination);
+  }
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image={destination.data.image}
-              title={destination.name}
-            />
-            <CardContent>
-              <Typography type="display2" component="h2">
-                {destination.name}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button dense color="primary">
-                Add to Favorites
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={9}>
-          <Paper>
-            <DetailsTab yelpName={destination.data.yelpName} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={3}>
-          <Paper className={classes.paper}>
-            {destination.data.description}
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+  renderContent() {
+    const { classes, destination } = this.props;
+    switch (this.props.destination) {
+      case null:
+        return;
+      default:
+        return (
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.media}
+                  image={destination.image}
+                  title={destination.name}
+                />
+                <CardContent>
+                  <Typography type="display2" component="h2">
+                    {destination.name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button dense color="primary">
+                    Add to Favorites
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={9}>
+              <Paper>
+                <DetailsTab yelpName={destination.yelpName} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Paper className={classes.paper}>{destination.description}</Paper>
+            </Grid>
+          </Grid>
+        );
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+    return <div className={classes.root}>{this.renderContent()}</div>;
+  }
 }
 
 DestinationDetails.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(DestinationDetails);
+function mapStateToProps({ destination }) {
+  return { destination };
+}
+
+export default connect(mapStateToProps, actions)(
+  withStyles(styles)(DestinationDetails)
+);
