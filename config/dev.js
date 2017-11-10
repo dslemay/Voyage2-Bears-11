f3d58babf27458804e0ae85846c8d7c46083fea6
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-10 07:03:11 -0800

    Remove dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    yelpClientID: 'qknZt5qr21xGZqGVWqqdTg',
-    yelpClientSecret:
-      'ekbKQq9c6LL6iamM3PO3wCG2uMZbcTpw3uJt03pESmZtZycoLE35i4p1iiI89Xld',
-    database:
-      'mongodb://bears11-team:wBBWPJWyQa-22Yh/a.k6F9Y)@ds155684.mlab.com:55684/chingu-travel',
-    cookieSecret: 'vniu4waor3j/cln4awo3ejd3aw',
-    googleFlights: 'AIzaSyCqLj6JPkZgBK8E9QZtgWBAO9OcVYaQrKc'
-  };
