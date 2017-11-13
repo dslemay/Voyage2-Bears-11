da531d2425f673855c8a47f892d62430cbb0e056
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-12 21:20:45 -0800

    Style CircleLoader component

--- client/src/components/CircleLoader.js
+++ client/src/components/CircleLoader.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { CircularProgress } from 'material-ui/Progress';
+import { withStyles } from 'material-ui/styles';
+import PropTypes from 'prop-types';
+
+const styles = theme => ({
+  root: {
+    padding: '30px 0',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
 
 function CircleLoader(props) {
+  const { classes } = props;
+  var loader;
   if (props.large) {
-    return <CircularProgress size={180} thickness={4} />;
+    loader = <CircularProgress size={180} thickness={4} />;
+  } else {
+    loader = <CircularProgress size={70} />;
   }
-  return <CircularProgress size={70} />;
+
+  return <div className={classes.root}>{loader}</div>;
 }
 
-export default CircleLoader;
+CircleLoader.PropTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(CircleLoader);
