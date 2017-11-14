641d3af3fbfdfbbc0ec8dce291187aa862498c32
Daniel Lemay <d.s.lemay@icloud.com>
2017-11-14 15:29:42 -0800

    Remove flightsReducer from application
    The functionality provided by the flightsReducer has been rolled into
    the destinationDetailsReducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import flightsReducer from './flightsReducer';
 import authReducer from './authReducer';
 import favoritesReducer from './favoritesReducer';
 import messageReducer from './messageReducer';
 import destinationDetailsReducer from './destinationDetailsReducer';
 
 export default combineReducers({
-  flights: flightsReducer,
   auth: authReducer,
   favorites: favoritesReducer,
   messages: messageReducer,
