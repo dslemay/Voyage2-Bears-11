8daa4b394a62bb52a3867dffbd1c3b00bf379d8d
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-03-15 22:35:34 -0700

    Add date functions for pickers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -20,20 +23,27 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <AlertMessages />
-          <Route exact path="/" component={Landing} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/details/:location" component={DestinationDetails} />
-          <Route path="/dashboard" component={DashTabs} />
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <AlertMessages />
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/details/:location" component={DestinationDetails} />
+            <Route path="/dashboard" component={DashTabs} />
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </MuiPickersUtilsProvider>
     );
   }
 }
+
+App.propTypes = {
+  fetchUser: PropTypes.func.isRequired,
+  fetchFavorites: PropTypes.func.isRequired,
+};
 
 export default connect(null, actions)(App);
